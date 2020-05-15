54918e64272657c7e33283e6b7b03699b28855af
Philip Kamenarsky <p.kamenarsky@gmail.com>
2020-05-15 17:48:31 +0300

    Error handling

--- src/Control/Monad/Lens/Simple.hs
+++ src/Control/Monad/Lens/Simple.hs
@@ -3,6 +3,7 @@
 
 module Control.Monad.Lens.Simple
   ( L()
+  , lensE
   , toL
   , fromL
   ) where
@@ -15,7 +16,7 @@ import Data.Profunctor
 
 -- | Monadic lenses.
 data L s u v = L {
-  get :: s -> v,
+  get :: s -> Either [String] v,
   put :: s -> u -> (s, Predicate s, v) }
   deriving Functor
 
@@ -25,36 +26,41 @@ map3 :: (c -> c') -> (a, b, c) -> (a, b, c')
 map3 f (a, b, c) = (a, b, f c)
 
 instance Profunctor (L s) where
-  dimap f g l = L (g . get l) (\s -> map3 g . put l s . f)
+  dimap f g l = L (fmap g . get l) (\s -> map3 g . put l s . f)
 
 instance Applicative (L s u) where
   pure = return
   (<*>) = ap
 
 instance Monad (L s u) where
-  return y = L (\ _ -> y) (\ s _ -> (s, \ s -> True, y))
+  return y = L (\ _ -> Right y) (\ s _ -> (s, \_ -> True, y))
 
   ly >>= kz = L getter putter
     where
-      getter s = get (kz (get ly s)) s
+      getter s = do
+        a <- get ly s
+        get (kz a) s
       putter s x = let (s', vy, y) = put ly s x
                        (s'', vz, z) = put (kz y) s' x
                    in (s'', liftA2 (&&) vy vz, z)
 
+lensE :: (s -> Either [String] v) -> (s -> v -> s) -> Lens s s (Either [String] v) (Either [String] v)
+lensE get put = Lens.lens get put'
+  where
+    put' s (Right a) = put s a
+    put' s _ = s
+
 -- | Conversion from standard 'Lens' to 'L'.
-toL :: Eq v => Lens s s v v -> L s v v
+toL :: Eq v => Lens s s (Either [String] v) (Either [String] v) -> L s v v
 toL l = L getter putter
   where
     getter = Lens.view l
     putter s x =
-      let s' = Lens.set l x s
-      in (s', \s' -> Lens.view l s' == x, x)
-
-fst3 :: (a, b, c) -> a
-fst3 (a, _, _) = a
+      let s' = Lens.set l (Right x) s
+      in (s', \s' -> Lens.view l s' == Right x, x)
 
 -- | Conversion from 'L' to standard 'Lens'.
-fromL :: L s u v -> Lens s s v u
-fromL l = Lens.lens (get l) (\s x ->
-  let (s', validate, _) = put l s x in
-  if validate s' then s' else error "Put conflict")
+fromL :: L s u v -> Lens s s (Either [String] v) (Either [String] u)
+fromL l = Lens.lens (get l) $ \s x -> case x of
+  Right x' -> let (s', validate, _) = put l s x' in if validate s' then s' else error "Put conflict"
+  Left _ -> s
